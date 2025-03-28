a98d14384e5be04e77e9bbdc1bffca89221d9389
MrrrBoB <10805477@my.uvu.edu>
2025-03-28 14:44:48 -0600

    Added a menu template and a simple starting object for player character

---
+++ Cryptid 1.0.1/Assets/Scripts/Utility/PauseBehavior.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PauseBehavior : MonoBehaviour
+{
+    public bool isPaused;
+
+    private void Start()
+    {
+        isPaused = false;
+    }
+
+    public void SetPause(bool setBool)
+    {
+        Time.timeScale = setBool ? 0 : 1;
+        isPaused = setBool;
+        print(isPaused);
+    }
+
+    public void TogglePause()
+    {
+        SetPause(!isPaused);
+    }
+}
