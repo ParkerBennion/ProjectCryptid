468005ea2f61622f0529b4a3c802d90143b5eea1
MrrrBoB <10805477@my.uvu.edu>
2025-06-04 17:45:31 -0600

    fixed the teleport issue

--- Cryptid 1.0.1/Assets/LevelBuilder/ProceduralBuilder/SpeedNavMeshHandler.cs
+++ Cryptid 1.0.1/Assets/LevelBuilder/ProceduralBuilder/SpeedNavMeshHandler.cs
@@ -3,31 +3,38 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class TraverseLinkSmoothly : MonoBehaviour
+public class SpeedNavMeshHandler : MonoBehaviour
 {
     public NavMeshAgent agent;
     private bool isTraversing;
+    private Animator animator;
     private Coroutine traverseRoutine;
     private float traverseDuration, elapsed;
     private Vector3 startPosition, endPosition;
     private WaitForEndOfFrame WFF;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
     private void Awake()
     {
         isTraversing = false;
+        animator = GetComponent<Animator>();
     }
 
-    void LateUpdate()
+    void Update()
     {
-        if (!isTraversing && IsApproachingLink())
+        if (!isTraversing && agent.isOnOffMeshLink)
             StartCoroutine(MoveOverLink());
+        else if(!isTraversing)
+            animator.SetFloat(Speed, agent.velocity.magnitude);
     }
 
     private IEnumerator MoveOverLink()
     {
+        animator.SetFloat(Speed, agent.speed);
         isTraversing = true;
         startPosition = transform.position;
         endPosition = agent.currentOffMeshLinkData.endPos+Vector3.up*agent.baseOffset;
-        traverseDuration = Vector3.Distance(startPosition, endPosition) / (agent.speed * .5f);
+        traverseDuration = Vector3.Distance(startPosition, endPosition) / agent.speed;
         elapsed = 0f;
         while (elapsed < traverseDuration)
         {
@@ -35,21 +42,10 @@ public class TraverseLinkSmoothly : MonoBehaviour
             elapsed += Time.deltaTime;
             yield return WFF;
         }
-
         agent.transform.position = endPosition;
         agent.CompleteOffMeshLink();
 
         isTraversing = false;
-    }
-    private bool IsApproachingLink()
-    {
-        if (agent.isOnOffMeshLink) return true;
-
-        OffMeshLinkData data = agent.currentOffMeshLinkData;
-        if (!data.valid) return false;
-
-        float distToStart = Vector3.Distance(agent.transform.position, data.startPos);
-        return distToStart < 3f; // tweak threshold
     }
     private void OnDestroy()
     {
