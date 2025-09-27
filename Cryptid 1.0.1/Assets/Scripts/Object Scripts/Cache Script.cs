e6eae6bc2c23e2909dabb80f74e7619e20d2c54d
MrrrBoB <10805477@my.uvu.edu>
2025-09-27 14:56:12 -0600

    Got loot chest working

--- Cryptid 1.0.1/Assets/Scripts/Object Scripts/Cache Script.cs
+++ Cryptid 1.0.1/Assets/Scripts/Object Scripts/Cache Script.cs
@@ -1,27 +1,42 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
-public class CacheScript : MonoBehaviour
+public class CacheScript : MonoBehaviour, IDamageable
 {
     [SerializeField] private GameObject[] lootPool;
     private Animator animator;
     private Vector3 lootSpawnOffset;
+    private bool hasOpened;
+    public UnityEvent openEvent;
     [SerializeField] private Vector3 lootTrajectoryForce;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         lootSpawnOffset = new Vector3(0, .5f, 0);
+        hasOpened = false;
     }
 
     public void GenerateLoot()
     {
         GameObject lootInstance = Instantiate(lootPool[Random.Range(0, lootPool.Length)],
-            lootSpawnOffset + gameObject.transform.position, Quaternion.identity);
+            lootSpawnOffset + gameObject.transform.position, transform.rotation);
         if (lootInstance.TryGetComponent(out Rigidbody rigidbody))
         {
             rigidbody.AddForce(transform.TransformPoint(lootTrajectoryForce));
+        }
+    }
+
+    public void DealDamage(float damage)
+    {
+        print("Cache hit");
+        if(!hasOpened)
+        {
+            openEvent.Invoke();
+            GenerateLoot();
+            //hasOpened = true;
         }
     }
 }
