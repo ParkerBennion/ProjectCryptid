3b3643c55304cab4f8f073167b497083f7a9564d
MrrrBoB <10805477@my.uvu.edu>
2025-09-28 00:17:23 -0600

    Cleaned the hell out of my code

--- Cryptid 1.0.1/Assets/Scripts/Utility/CameraDitherer.cs
+++ Cryptid 1.0.1/Assets/Scripts/Utility/CameraDitherer.cs
@@ -4,42 +4,23 @@ using UnityEngine;
 
 public class CameraDitherer : MonoBehaviour
 {
-    private RaycastHit[] raycastHits;
-    private Ray ray;
-    private float castDistance;
-    [SerializeField] private LayerMask mask;
-    [SerializeField] private List<GameObject> obstacleList;
     private MaterialPropertyBlock matBlock;
-    private new MeshRenderer renderer;
 
     private void Awake()
     {
-        ray = new Ray(gameObject.transform.position, Vector3.forward);
         matBlock = new MaterialPropertyBlock();
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("DitherObject"))
         {
-            renderer = other.GetComponent<MeshRenderer>();
-            if(renderer ==null)
-                return;
-            renderer.GetPropertyBlock(matBlock);
-            matBlock.SetFloat("_Dithering",1f);
-            renderer.SetPropertyBlock(matBlock);
+            if (other.TryGetComponent(out MeshRenderer renderer))
+            {
+                renderer.GetPropertyBlock(matBlock);
+                matBlock.SetFloat("_Dithering",1f);
+                renderer.SetPropertyBlock(matBlock);
+            }
         }
     }
 
@@ -47,12 +28,12 @@ public class CameraDitherer : MonoBehaviour
     {
         if (other.CompareTag("DitherObject"))
         {
-            renderer = other.GetComponent<MeshRenderer>();
-            if(renderer ==null)
-                return;
-            renderer.GetPropertyBlock(matBlock);
-            matBlock.SetFloat("_Dithering",2f);
-            renderer.SetPropertyBlock(matBlock);
+            if (other.TryGetComponent(out MeshRenderer renderer))
+            {
+                renderer.GetPropertyBlock(matBlock);
+                matBlock.SetFloat("_Dithering",2f);
+                renderer.SetPropertyBlock(matBlock);
+            }
         }
     }
 }
