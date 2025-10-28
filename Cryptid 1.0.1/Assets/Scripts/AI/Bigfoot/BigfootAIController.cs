cb4433a7d444aeb03b23332d3218b12e7c0a9de7
MrrrBoB <10805477@my.uvu.edu>
2025-10-27 19:35:37 -0600

    bigfoot code almost done, He just needs to flee the character

--- Cryptid 1.0.1/Assets/Scripts/AI/Bigfoot/BigfootAIController.cs
+++ Cryptid 1.0.1/Assets/Scripts/AI/Bigfoot/BigfootAIController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
@@ -11,5 +12,26 @@ public class BigfootAIController : MonoBehaviour
     public void Flee()
     {
         animator.SetTrigger("Flee");
+    }
+
+    public void TurnToPlayer(GameObject player)
+    {
+        StartCoroutine(TurnAndFacePlayer(player));
+    }
+    public IEnumerator TurnAndFacePlayer(GameObject player)
+    {
+        WaitForEndOfFrame wff = new WaitForEndOfFrame();
+        float elapsedTime = 0;
+        agent.isStopped = true;
+        float turnTime = .35f;
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation =  Quaternion.LookRotation(player.transform.position - transform.position);
+        while (elapsedTime < turnTime)
+        {
+            yield return wff;
+            transform.rotation = Quaternion.Slerp(startRotation, endRotation, elapsedTime / turnTime);
+            print(elapsedTime);
+            elapsedTime += Time.deltaTime;
+        }
     }
 }
