fb3a524b80d8801a695eebdad947b41495974a3a
MrrrBoB <10805477@my.uvu.edu>
2026-02-05 17:54:38 -0700

    Super rough dialogue system, still needs some serious testing

---
+++ Cryptid 1.0.1/Assets/Scripts/UIScripts/NPC_DialogueModule.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NPC_DialogueModule : MonoBehaviour
+{
+    [SerializeField] private SimpleTextUpdater textDisplay;
+    public string[] activeConversationNode;
+    [SerializeField]private int currentLineIndex;
+    [SerializeField]private UnityEvent conversationStartEvent, conversationEndEvent;
+    private string[] defaultConversation = new[]
+    {
+        "Good to see ya",
+        "My code isn't finished yet...",
+        "...so good luck out there"
+    };
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DelayedStart());
+    }
+
+    
+    public void NextLine()
+    {
+        
+        if (currentLineIndex >= activeConversationNode.Length-1)
+        {
+            conversationEndEvent.Invoke();
+        }
+        else
+        {
+            currentLineIndex++;
+            textDisplay.updateText(activeConversationNode[currentLineIndex]);
+        }
+    }
+
+    public void PreviousLine()
+    {
+        
+        if (currentLineIndex <= 0)
+            return;
+        currentLineIndex--;
+        textDisplay.updateText(activeConversationNode[currentLineIndex]);
+    }
+
+    public void EndConversation()
+    {
+        conversationEndEvent.Invoke();
+    }
+
+    private IEnumerator DelayedStart()
+    {
+        conversationStartEvent.Invoke();//disables hud and controls
+        activeConversationNode = defaultConversation;//assign conversation chain
+        currentLineIndex = 0;
+        yield return new WaitForSeconds(1);
+        textDisplay.updateText(activeConversationNode[currentLineIndex]);
+    }
+    
+    
+    
+}
