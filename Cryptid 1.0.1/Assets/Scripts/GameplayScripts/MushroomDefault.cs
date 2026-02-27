82608bfa1413277558501dd143577d4c3855a4ac
MrrrBoB <10805477@my.uvu.edu>
2026-02-26 17:43:21 -0700

    made mushroom inventory item, needs to be fixed not updating inventory screen

--- Cryptid 1.0.1/Assets/Scripts/GameplayScripts/MushroomDefault.cs
+++ Cryptid 1.0.1/Assets/Scripts/GameplayScripts/MushroomDefault.cs
@@ -3,12 +3,12 @@ using UnityEngine.Events;
 
 public class MushroomDefault : MonoBehaviour, IDamageable
 {
-    [SerializeField] private GameActionFloat changeHealthAction;
+    [SerializeField] private GameObject pickupPrefab;
     [SerializeField] private GameObject particlePrefab;
     [SerializeField] private float healAmount;
     public void DealDamage(float damage)
     {
-        changeHealthAction?.RaiseAction(healAmount);
+        Instantiate(pickupPrefab, transform.position, Quaternion.identity);
         Instantiate(particlePrefab, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
