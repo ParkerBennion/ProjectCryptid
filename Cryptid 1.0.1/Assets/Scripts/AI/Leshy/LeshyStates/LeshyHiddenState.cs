264efb5488575487e69c3872d04a01d51c663842
MrrrBoB <10805477@my.uvu.edu>
2026-04-03 21:14:44 -0600

    leshy alerted behavior

---
+++ Cryptid 1.0.1/Assets/Scripts/AI/Leshy/LeshyStates/LeshyHiddenState.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class LeshyHiddenState : State
+{
+    public override void LogicUpdate()
+    {
+        
+    }
+
+    public override void OnEnterState()
+    {
+        navAgent.isStopped = true;
+    }
+
+    public override void OnExitState()
+    {
+        navAgent.isStopped = false;
+    }
+
+    public void EngagePlayer()
+    {
+        animator.SetTrigger("Alerted");
+    }
+
+    public override void OnAnimationFinish()
+    {
+        base.OnAnimationFinish();
+        navAgent.isStopped = false;
+    }
+}
--- Cryptid 1.0.1/Assets/Scripts/AI/State.cs
+++ Cryptid 1.0.1/Assets/Scripts/AI/State.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.AI;
-[CreateAssetMenu]
 public abstract class State : MonoBehaviour
 {
     
