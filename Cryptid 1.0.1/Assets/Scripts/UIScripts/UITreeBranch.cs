77ed3f47c70a877bb7f4c4caef34369dc7326dd7
MrrrBoB <10805477@my.uvu.edu>
2026-04-25 17:00:02 -0600

    leaf framework is finished

--- Cryptid 1.0.1/Assets/Scripts/UIScripts/UITreeBranch.cs
+++ Cryptid 1.0.1/Assets/Scripts/UIScripts/UITreeBranch.cs
@@ -1,28 +1,37 @@
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using Random = UnityEngine.Random;
 
 public class UITreeBranch : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField] private RectTransform xForm;
     private RectTransform canvasXform;
     [SerializeField] private Canvas parentCanvas;
+    [SerializeField] private UnityEvent startEvent;
     private Vector2 offsetVector, localVector, tempVector, homeVector;
+    private Vector3 rotationVector;
     private float startY;
     private Coroutine returningRoutine;
 
-    [Header("ClampingInfo")] [SerializeField] 
+    [Header("ClampingInfo")] [SerializeField]
     private bool swipesRight;
+
     [SerializeField] private float returnTime, swipeThreshold, swipeSuccessTarget, minX, maxX;
 
+    [SerializeField] private UnityEvent successEvent, completeEvent;
+
+    [Header("Visual")] [SerializeField] private RectTransform branchVisual;
+    [SerializeField] private float startRotation, endRotation, restingRotation;
 
     private void Awake()
     {
-        canvasXform= parentCanvas.transform as RectTransform;
+        canvasXform = parentCanvas.transform as RectTransform;
         startY = xForm.localPosition.y;
         homeVector = xForm.anchoredPosition;
-        if(swipesRight)
+        if (swipesRight)
         {
             minX = xForm.localPosition.x;
             maxX = xForm.localPosition.x + swipeThreshold;
@@ -32,12 +41,20 @@ public class UITreeBranch : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
             maxX = xForm.localPosition.x;
             minX = xForm.localPosition.x - swipeThreshold;
         }
+
+        rotationVector = Vector3.zero;
         print(homeVector);
     }
 
-    public void OnBeginDrag(PointerEventData eventData)//get the touch location in the canvas
+    private void OnEnable()
     {
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(xForm, 
+        branchVisual.gameObject.SetActive(true);
+        StartCoroutine(SpawnFromOffScreen());
+    }
+
+    public void OnBeginDrag(PointerEventData eventData) //get the touch location in the canvas
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(xForm,
             eventData.position,
             eventData.pressEventCamera,
             out offsetVector);
@@ -55,18 +72,24 @@ public class UITreeBranch : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
                 eventData.pressEventCamera,
                 out localVector))
         {
-            tempVector = localVector-offsetVector;
+            tempVector = localVector - offsetVector;
             tempVector.y = startY;
-            tempVector.x= Mathf.Clamp(tempVector.x, minX, maxX);
+            tempVector.x = Mathf.Clamp(tempVector.x, minX, maxX);
             xForm.localPosition = tempVector;
+            rotationVector.z = Mathf.Lerp(startRotation, endRotation,
+                Mathf.Abs(xForm.localPosition.x - homeVector.x) / swipeSuccessTarget);
+            branchVisual.eulerAngles = rotationVector;
         }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        print(xForm.localPosition.x-homeVector.x);
-        if(Mathf.Abs(xForm.localPosition.x-homeVector.x)>swipeSuccessTarget)
-            print("Successful Swipe");
+        print((xForm.localPosition.x - homeVector.x) / swipeSuccessTarget); // use to lerp
+        if (Mathf.Abs(xForm.localPosition.x - homeVector.x) > swipeSuccessTarget)
+        {
+            successEvent?.Invoke();
+            StartCoroutine(ContinueOffScreen());
+        }
         else
             returningRoutine = StartCoroutine(ReturnToHome());
     }
@@ -76,12 +99,63 @@ public class UITreeBranch : MonoBehaviour, IBeginDragHandler, IDragHandler, IEnd
         print(homeVector);
         Vector2 startVector = xForm.localPosition;
         float elapsedTime = 0;
+        float startingRotation = Mathf.DeltaAngle(0f, branchVisual.eulerAngles.z);
+        print(startingRotation);
         while (elapsedTime < returnTime)
         {
-            xForm.localPosition = Vector2.Lerp(startVector,homeVector,elapsedTime/returnTime);
+            xForm.localPosition = Vector2.Lerp(startVector, homeVector, elapsedTime / returnTime);
+            rotationVector.z = Mathf.Lerp(startingRotation, 0,
+                elapsedTime / returnTime);
+            branchVisual.eulerAngles = rotationVector;
             elapsedTime += Time.deltaTime;
             yield return null;
         }
+
         xForm.localPosition = homeVector;
     }
+
+    private IEnumerator ContinueOffScreen()
+    {
+        float elapsedTime = 0;
+        float startingRotation = Mathf.DeltaAngle(0f, branchVisual.eulerAngles.z);
+        while (elapsedTime < 1f)
+        {
+            rotationVector.z = Mathf.Lerp(startingRotation, restingRotation,
+                (elapsedTime / 1f));
+            branchVisual.eulerAngles = rotationVector;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        branchVisual.gameObject.SetActive(false);
+        completeEvent?.Invoke();
+    }
+
+    private IEnumerator SpawnFromOffScreen()
+    {
+        float delay = Random.Range(0f, .25f);
+        float overShoot = swipesRight ? 25f:-25f;
+        float elapsedTime = 0;
+        yield return new WaitForSeconds(delay);
+        while (elapsedTime<.25f)//how long it takes to reach the first point
+        {
+            rotationVector.z = Mathf.Lerp(restingRotation, overShoot,
+                elapsedTime / .25f);
+            branchVisual.eulerAngles = rotationVector;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        elapsedTime = 0;
+        while (elapsedTime<.25f)//how long it takes to reach the second point
+        {
+            rotationVector.z = Mathf.Lerp(overShoot, 0f,
+                elapsedTime / .25f);
+            branchVisual.eulerAngles = rotationVector;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        branchVisual.eulerAngles=Vector3.zero;
+        startEvent?.Invoke();
+    }
+
 }
