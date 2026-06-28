b0fa2bd2aa164ea16c2e996fd4401f28911f898e
MrrrBoB <10805477@my.uvu.edu>
2026-06-28 13:59:37 -0600

    decorated first scene

--- Cryptid 1.0.1/Assets/Scripts/GameplayScripts/SpawnRandomItem.cs
+++ Cryptid 1.0.1/Assets/Scripts/GameplayScripts/SpawnRandomItem.cs
@@ -6,6 +6,7 @@ public class SpawnRandomItem : MonoBehaviour
 {
     [SerializeField] protected GameObject[] spawnList;
     [SerializeField] private bool SpawnOnCreation;
+    [SerializeField] private bool randomRotation;
 /// <summary>
 /// Spawns a random object from spawn list as a child of this object
 /// </summary>
@@ -17,7 +18,10 @@ public class SpawnRandomItem : MonoBehaviour
 
     public virtual void SpawnItemFromList()
     {
-        Instantiate(spawnList[Random.Range(0, spawnList.Length)], transform.position, Quaternion.identity, this.transform);
+        if(randomRotation)
+            Instantiate(spawnList[Random.Range(0, spawnList.Length)], transform.position, Quaternion.Euler(Vector3.up*Random.Range(0f,360f)), this.transform);
+        else
+            Instantiate(spawnList[Random.Range(0, spawnList.Length)], transform.position, Quaternion.identity, this.transform);
     }
 
 }
--- Cryptid 1.0.1/Assets/Scripts/Object Scripts/GeoCacheSpawner.cs
+++ Cryptid 1.0.1/Assets/Scripts/Object Scripts/GeoCacheSpawner.cs
@@ -1,14 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using Random = UnityEngine.Random;
 
 public class GeoCacheSpawner : MonoBehaviour
 {
-    [SerializeField] private GameObject cacheObject;
+    [SerializeField] private GameObject cacheObjectPrefab;
+    private GeoCache cacheScript;
     [SerializeField] private float spawnRange;
+    public UnityEvent cacheDestroyedEvent;
     private void Start()
     {
-        Instantiate(cacheObject, transform.TransformPoint(new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange,spawnRange))), Quaternion.identity);
+       cacheScript = Instantiate(cacheObjectPrefab,
+           transform.TransformPoint(new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange,spawnRange))),
+           Quaternion.identity,
+           transform).GetComponent<GeoCache>();
+       cacheScript.spawner = this;
     }
     
 }
